c12d5b24de35e0fafa7e9dc3d408979d7e908ae0
Jeff Haack <jeff.haack@gmail.com>
2012-11-06 19:09:39 +0400

    grrr

--- Gemfile
+++ Gemfile
@@ -5,8 +5,10 @@ gem 'rails', '3.2.8'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'bundle'
+gem 'bundler'
 gem 'sqlite3'
-
+gem 'capistrano'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ config/deploy.rb
@@ -0,0 +1,48 @@
+require 'bundler/capistrano'
+
+set :application, "testcap"
+set :repository,  "git@github.com:jeffhaack/testcap.git"
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "108.166.79.19"                          # Your HTTP server, Apache/etc
+role :app, "108.166.79.19"                          # This may be the same as your `Web` server
+role :db,  "108.166.79.19", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+set :user, "root"
+set :group, "root"
+set :deploy_to, "/var/www/testcap"
+set :use_sudo, false
+
+set :deploy_via, :copy
+set :copy_strategy, :export
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  desc "Restart the application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  desc "Copy the database.yml file into the latest release"
+  task :copy_in_database_yml do
+    run "cp #{shared_path}/config/database.yml #{latest_release}/config/"
+  end
+end
+before "deploy:assets:precompile", "deploy:copy_in_database_yml"
