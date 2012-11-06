84efda686a5ab76289df82780b0ea76a86f8430b
root <root@rails-postgis-go.(none)>
2012-11-06 14:57:33 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testcap::Application.config.secret_token = 'ec2fd53ab109f87089efded5fcc9c2c04d2e1b93975ff238d83d1a5d4c134ae15a4343e9ebb17d484069e1dc42c53a56116352825d74d71f85639ffdd04052c4'
